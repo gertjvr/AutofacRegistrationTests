8ef23f764f01d13f9c0d91b81d55b80fcb947c9d
Gert Jansen van Rensburg <gertjvr@gmail.com>
2014-06-27 11:08:02 +1000

    File | New Project

---
+++ Conventions/AutofacContainerAssertion.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Autofac;
+using Autofac.Core;
+using AutofacRegistration.Tests.Extensions;
+
+namespace AutofacRegistration.Tests.Conventions
+{
+    public class AutofacContainerAssertion : AutofacBaseAssertion
+    {
+        private readonly Func<Type, bool> _filter;
+        private readonly Func<Type, bool> _isKnownOffender;
+
+        public AutofacContainerAssertion()
+            : this(type => true, type => true)
+        {
+        }
+
+        public AutofacContainerAssertion(Func<Type, bool> filter, Func<Type, bool> isKnownOffender)
+        {
+            if (filter == null) 
+                throw new ArgumentNullException("filter");
+            
+            if (isKnownOffender == null) 
+                throw new ArgumentNullException("isKnownOffender");
+
+            _filter = filter;
+            _isKnownOffender = isKnownOffender;
+        }
+
+        public override void Verify(IContainer container)
+        {
+            var distinctTypes = container.ComponentRegistry.Registrations
+                .SelectMany(r => r.Services.OfType<TypedService>().Select(s => s.ServiceType).Union(GetGenericFactoryTypes(r)))
+                .Where(t => _filter(t))
+                .Distinct();
+
+            var exceptions = new List<Exception>();
+            foreach (var distinctType in distinctTypes)
+            {
+                if (_isKnownOffender(distinctType))
+                {
+                    Console.WriteLine("'{0}' is known not to resolve correctly - ignoring it.", distinctType.ToTypeNameString());
+                    return;
+                }
+
+                try
+                {
+                    container.Resolve(distinctType);
+                }
+                catch (DependencyResolutionException e)
+                {
+                    if (e.Message.Contains("No scope with a Tag"))
+                    {
+                        Console.WriteLine("'{0}' failed to resolve 'No scope with a Tag' - The test is considered inconclusive.", distinctType.ToTypeNameString());
+                        return;
+                    }
+
+                    exceptions.Add(e);
+                }
+            }
+
+            if (exceptions.Any())
+                throw new AggregateException("Can't resolve all types registered with Autofac", exceptions);
+        }
+    }
+}
---
+++ Scenarios.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using Autofac;
+using Autofac.Core;
+using AutofacRegistration.Tests.Conventions;
+using NUnit.Framework;
+
+namespace AutofacRegistration.Tests
+{
+    public interface IBarService
+    {
+    }
+
+    public interface IFooService
+    {
+    }
+
+    public interface IBoomViewModel
+    {
+    }
+
+    public class BarService : IBarService
+    {
+    }
+
+    public class FooService : IFooService
+    {
+        private readonly IBarService _barService;
+
+        public FooService(IBarService barService)
+        {
+            _barService = barService;
+        }
+    }
+
+    public class BoomViewModel : IBoomViewModel
+    {
+        private readonly IBarService _barService;
+
+        public delegate BoomViewModel Factory(Guid id);
+
+        public BoomViewModel(Guid id, IBarService barService)
+        {
+            _barService = barService;
+        }
+    }
+
+    public class TestModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<FooService>()
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterType<BarService>()
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterType<BoomViewModel>()
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+        }
+    }
+
+    [TestFixture]
+    public class WhenTypesAreRegister
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterModule(new TestModule());
+
+            _container = builder.Build();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _container.Dispose();
+        }
+
+        private IContainer _container;
+
+        [Test]
+        public void ShouldResolveBoomToNotThrowException()
+        {   
+            using (var scope = _container.BeginLifetimeScope())
+            {
+                _container.Resolve<IBoomViewModel>(new NamedParameter("id", Guid.NewGuid()));
+            }
+        }
+
+        [Test]
+        public void ShouldResolveBoomToThrowException()
+        {
+            Assert.Catch<DependencyResolutionException>(() =>
+            {
+                using (var scope = _container.BeginLifetimeScope())
+                {
+                    _container.Resolve<IBoomViewModel>();
+                }
+            });
+        }
+
+        [Test]
+        public void ShouldResolveFoo()
+        {
+            using (var scope = _container.BeginLifetimeScope())
+            {
+                _container.Resolve<IFooService>();
+            }
+        }
+
+        [Test]
+        public void ShouldResolveAllTypesInContainer()
+        {
+            var assertion = new AutofacContainerAssertion(Filter, IsKnownOffender);
+            assertion.Verify(_container);
+        }
+        
+        private bool Filter(Type serviceType)
+        {
+            return true;
+        }
+
+        private bool IsKnownOffender(Type serviceType)
+        {
+            return false;
+        }
+    }
+}
